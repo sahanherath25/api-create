9aed79c60000755e54675fa163cb33f5fc29fba9
Sahan Udara <sahanherath444@gmail.com>
2024-08-09 17:22:35 +0530

    Removed spaces -Sahan

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ if(process.env.NODE_ENV==="development"){
   app.use(morgan('dev'))
 }
 
-
-
 //TODO Importing Routers
 const tourRouter=require("./routes/tourRoutes")
 const userRouter=require("./routes/userRoutes")
@@ -27,13 +25,9 @@ const AppError = require('./utils/appError');
 
 const globalErrorHandler=require("./controllers/errorControllers")
 
-
-
 //TODO Mounting Routers
 app.use("/api/v1/tours",tourRouter);
 app.use("/api/v1/users",userRouter);
-
-
 
 app.all("*",(req, res, next)=>{
   // res.status(404).json({
